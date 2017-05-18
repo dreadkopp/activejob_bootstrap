dea546a4a3cf0e9701ba16165b76e2f4e0623454
Daniel Musketa <dm@xanadu.dd.activ-job.intra>
2017-05-18 20:32:34 +0200

    created ListView and DetailView for jobs

--- activejob/activejob/jobs/views.py
+++ activejob/activejob/jobs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Job
+
+
+class JobDetailView(DetailView):
+    model = Job
+    template_name = "web/pages/stellenmarkt/job-detail.html"
+
+
+class JobListView(ListView):
+    model = Job
+    template_name = "web/pages/stellenmarkt.html"
+    paginate_by = 10
