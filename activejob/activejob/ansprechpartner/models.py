92a0498b9e1927239965058e11a849e6f6802da7
Arne <arne@yoshi.dynu.com>
2017-05-21 16:18:00 +0200

    ansprechpartner/berufsfelder/kompetenzbereiche/vorteile templates

--- activejob/activejob/ansprechpartner/models.py
+++ activejob/activejob/ansprechpartner/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
 class Ansprechpartner(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
+    field = models.CharField(max_length=100)
+    banner_color = models.CharField(max_length=6)
+    banner_slogan = models.CharField(max_length=200)
+    contacts = models.ManyToManyField(Contact)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    mail = models.EmailField()
+
+    location = models.ForeignKey("Location")
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    @property
+    def imageurl(self):
+        return "{}.{}.jpg".format(self.first_name, self.last_name)
