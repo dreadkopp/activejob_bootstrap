6db5e1345a97d0525ca790ed91b25e8610f46586
Arne <arne@yoshi.dynu.com>
2017-05-21 23:22:06 +0200

    edited 'Vorteile' class to contain all values used in template

--- activejob/activejob/vorteile/models.py
+++ activejob/activejob/vorteile/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class Pro(models.Model):
+    text = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return text
+
+
 class Vorteile(models.Model):
     title = models.CharField(max_length=150)
     subtitle = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    pros = models.ForeignKey("Pro")
     banner_color = models.CharField(max_length=6)
     banner_slogan = models.CharField(max_length=200)
-    slub = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
