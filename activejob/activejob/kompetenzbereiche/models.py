ab6d50554bfc6fbe4351bf132d9278bc8a66e87d
Arne <arne@yoshi.dynu.com>
2017-05-21 23:29:25 +0200

    added/edited kompetenzbereiche/models.py to contain all values and objects used in template

--- activejob/activejob/kompetenzbereiche/models.py
+++ activejob/activejob/kompetenzbereiche/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return name
+
+class Bereich(models.Model):
+    title = models.CharField(max_length=200)
+    jobs = models.ForeignKey("Job")
+
+    def __str__(self):
+        return title
+
+
 class Kompetenzbereiche(models.Model):
     title = models.CharField(max_length=150)
     subtitle = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    fields = models.ForeignKey("Bereich")
     banner_color = models.CharField(max_length=6)
     banner_slogan = models.CharField(max_length=200)
     slug = models.CharField(max_length=100)
