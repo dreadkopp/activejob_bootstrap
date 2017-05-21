835d0e9ad88eb4c5b93f2af8696e21234c4851da
Arne <arne@yoshi.dynu.com>
2017-05-21 23:34:23 +0200

    added needed values and objects used by template to berufsfelder/models.py

--- activejob/activejob/berufsfelder/models.py
+++ activejob/activejob/berufsfelder/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from kompetenzbereiche.models import Job,Bereich
 
 class Berufsfelder(models.Model):
     title = models.CharField(max_length=150)
-    subtitel = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    fields = models.ForeignKey("Bereich")
     banner_color = models.CharField(max_length=6)
     banner_slogan = models.CharField(max_length=200)
     slug = models.CharField(max_length=100)
