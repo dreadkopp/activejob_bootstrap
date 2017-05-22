33a59120563e555ed622961195bbc7216220129a
Arne <arne@yoshi.dynu.com>
2017-05-22 16:08:40 +0200

    guess i should stay away from stuff i don't fully understand :P

--- activejob/activejob/menus/models.py
+++ activejob/activejob/menus/models.py
@@ -5,16 +5,16 @@ class MenuItem:
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=100)
     active = models.BooleanField
-    sublist = models.ForeignKey(MenuItem)
+    #sublist = models.ForeignKey(MenuItem)
 
 class TopMenu:
-    menuItems = models.ForeignKey(MenuItem)
+    #menuItems = models.ForeignKey(MenuItem)
 
     def __str__(self):
         return menuItems
 
 class LeftMenu:
-    menuItems = models.ForeignKey(MenuItem)
+    #menuItems = models.ForeignKey(MenuItem)
 
     def __str__(self):
         return menuItems
