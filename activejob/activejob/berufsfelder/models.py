ec8fe3d3227358554e5fcd5a92dfd0ae51c8ab4f
Arne <arne@yoshi.dynu.com>
2017-05-22 15:22:41 +0200

    fixed 'berufsfelder' class

--- activejob/activejob/berufsfelder/models.py
+++ activejob/activejob/berufsfelder/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from kompetenzbereiche.models import Job,Bereich
+from kompetenzbereiche.models import Bereich
 
 class Berufsfelder(models.Model):
     title = models.CharField(max_length=150)
     text = models.CharField(max_length=1000)
-    fields = models.ForeignKey("Bereich")
+    fields = models.ForeignKey(Bereich)
     banner_color = models.CharField(max_length=6)
     banner_slogan = models.CharField(max_length=200)
     slug = models.CharField(max_length=100)
