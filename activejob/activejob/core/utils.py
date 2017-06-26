dc28f677652935f84f0331b3ad9aeaa22779da4e
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-26 17:39:26 +0200

    added menu_items for menu_left

--- activejob/activejob/core/utils.py
+++ activejob/activejob/core/utils.py
@@ -2,25 +2,97 @@ def build_main_menu (active_node=None, submenu=None):
     menu_top = [
         {
             "name": "Home",
-            "active": True,
+            "active": active_node == "home",
             "url": "home",
             "class": "glyphicon glyphicon-home",
         },
         {
             "name": "Über uns",
             "url": "unternehmensprofil",
+            "active": active_node == "über uns",
         },
         {
             "name": "Unternehmen",
             "url": "home",
+            "active": active_node == "unternehmen",
         },
         {
             "name": "Bewerber",
             "url": "home",
+            "active": active_node == "bewerber",
         },
     ]
 
-    menu_left = []
+    menu_items = {
+        "über uns": [
+            {
+                "name": "Unternehmensprofil",
+                "url": "unternehmensprofil"
+            },
+            {
+                "name": "Leitbild",
+                "url": "leitbild"
+            },
+            {
+                "name": "Standorte",
+                "url": "standorte"
+            },
+            {
+                "name": "Kontakt",
+                "url": "kontakt"
+            },
+        ],
+        "unternehmen": [
+            {
+                "name": "Arbeitnehmerüberlassung",
+                "url": "arbeitnehmerueberlassung"
+            },
+            {
+                "name": "Personalvermittlung",
+                "url": "personalvermittlung"
+            },
+            {
+                "name": "Arbeitsvermittlung",
+                "url": "arbeitsvermittlung"
+            },
+            {
+                "name": "Personalanfrage",
+                "url": "personalanfrage"
+            },
+            {
+                "name": "Personalauswahl",
+                "url": "personalauswahl"
+            },
+        ],
+        "bewerber": [
+            {
+                "name": "Arbeitnehmerüberlassung",
+                "url": "arbeitnehmerueberlassung"
+            },
+            {
+                "name": "Personalvermittlung",
+                "url": "personalvermittlung"
+            },
+            {
+                "name": "Arbeitsvermittlung",
+                "url": "arbeitsvermittlung"
+            },
+            {
+                "name": "Karriereberatung",
+                "url": "karriereberatung"
+            },
+            {
+                "name": "Stellenmarkt",
+                "url": "stellenmarkt"
+            },
+            {
+                "name": "Karriere bei activjob",
+                "url": "karriere_activjob"
+            },
+        ],
+    }
+
+    menu_left = menu_items.get(active_node)
 
     return {
         "menu_top": menu_top,
