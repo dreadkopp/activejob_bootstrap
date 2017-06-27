a89fd3e37e44bd4e237b6455122316effa36a576
Arne <arne@yoshi.dynu.com>
2017-06-27 17:54:22 +0200

    berufsfelder done

--- activejob/activejob/berufsfelder/models.py
+++ activejob/activejob/berufsfelder/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from kompetenzbereiche.models import Bereich
 
 class Berufsfelder(models.Model):
     title = models.CharField(max_length=150)
     text = models.CharField(max_length=1000)
-    fields = models.ForeignKey(Bereich)
     banner_color = models.CharField(max_length=6)
     banner_slogan = models.CharField(max_length=200)
     slug = models.CharField(max_length=100)
+
+class Bereich(models.Model):
+    page = models.ForeignKey("Berufsfelder")
+    name = models.CharField(max_length=200)
+
+
+class Detail(models.Model):
+    sector = models.ForeignKey("Bereich")
+    name = models.CharField(max_length=200)
