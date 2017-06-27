6ca0d2cf0abda3e451b653a8417ee81183a85bdb
Arne <arne@yoshi.dynu.com>
2017-06-27 15:16:05 +0200

    corrected referenzen

--- activejob/activejob/referenzen/models.py
+++ activejob/activejob/referenzen/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Job(models.Model):
-    name = models.CharField(max_length=150)
-
 class Field(models.Model):
     name = models.CharField(max_length=150)
-    jobs = models.ForeignKey("Job")
+
+class Job(models.Model):
+    name = models.CharField(max_length=150)
+    field = models.ForeignKey("Field")
