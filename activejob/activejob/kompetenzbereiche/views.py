a9f4a2ff6599d588c3d518f14985e1a20316825f
Arne <arne@yoshi.dynu.com>
2017-06-27 17:41:33 +0200

    kompetenzbereiche done

--- activejob/activejob/kompetenzbereiche/views.py
+++ activejob/activejob/kompetenzbereiche/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from .models import Kompetenzbereich
 from jobs.mixins import QuickSearchFormMixin
 from core.utils import MenuMixin
 
-class KompetenzbereicheView(QuickSearchFormMixin, MenuMixin, ListView):
+class KompetenzbereicheView(QuickSearchFormMixin, MenuMixin, DetailView):
     model = Kompetenzbereich
     template_name = "web/pages/kompetenzbereiche.html"
