b29642d89423a56b375868ef4b061055c5c6c137
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-27 16:09:21 +0200

    referenzen.models: __str__

--- activejob/activejob/referenzen/models.py
+++ activejob/activejob/referenzen/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Field(models.Model):
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Job(models.Model):
     name = models.CharField(max_length=150)
     fields = models.ForeignKey("Field")
+
+    def __str__(self):
+        return self.name
