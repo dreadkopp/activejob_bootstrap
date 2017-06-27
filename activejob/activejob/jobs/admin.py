2f33bd3c074e980a799fcc685c3c5876c2c763c4
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-27 02:49:05 +0200

    enable admin for jobs app

--- activejob/activejob/jobs/admin.py
+++ activejob/activejob/jobs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category,
+    Company,
+    Contact,
+    Department,
+    Job,
+    Location,
+    State,
+)
+
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Contact)
+admin.site.register(Department)
+admin.site.register(Job)
+admin.site.register(Location)
+admin.site.register(State)
