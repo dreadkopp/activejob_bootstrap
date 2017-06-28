5d3bd996ab6531e7f582a08c2cae2f32ecfb5580
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-28 17:58:19 +0200

    add Personalanfrage to admin (and pluralize)

--- activejob/activejob/contactmessages/admin.py
+++ activejob/activejob/contactmessages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage, Personalanfrage
 
 admin.site.register(ContactMessage)
+admin.site.register(Personalanfrage)
--- activejob/activejob/contactmessages/models.py
+++ activejob/activejob/contactmessages/models.py
@@ -30,3 +30,6 @@ class Personalanfrage(models.Model):
 
     def __str__ (self):
         return "{} {}".format(self.company, self.job)
+
+    class Meta:
+        verbose_name_plural = "Personalanfragen"
