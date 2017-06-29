64526b68563c5cc2b1e65f2d4f649f7009fa2b9a
Arne <arne@yoshi.dynu.com>
2017-06-29 16:31:33 +0200

    something like this might work for the menu if this is possible

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -1,7 +1,18 @@
 from core.views import SearchAndMenuDetailView
 from .models import Page
+from core.utils import build_main_menu
 
 
 class PageView(SearchAndMenuDetailView):
+
+    def dispatch(self, request, *args, **kwargs):
+        self.active_nodes = {
+            "top": "Page.menu_top_entry",
+            "left": "Page.menu_left_entry",
+            "sub": "Page.menu_left_sub_entry",
+        }
+
+        return super().dispatch(request, *args, **kwargs)
+
     template_name = "web/pages/page.html"
     model = Page
