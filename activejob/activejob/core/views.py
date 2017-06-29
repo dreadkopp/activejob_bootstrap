d1df9fd07d893a75027bd537eb608d69d9aa92f6
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-29 09:45:19 +0200

    removed QuickSearchForm from SitemapView
    because it's already provided by QuickSearchFormMixin
    in SearchAndMenuTemplateView

--- activejob/activejob/core/views.py
+++ activejob/activejob/core/views.py
@@ -2,7 +2,7 @@ from django.views.generic import CreateView, DetailView, ListView, TemplateView
 from .mixins import MenuMixin
 from jobs.mixins import QuickSearchFormMixin
 from .utils import menu_items
-from jobs.forms import QuickSearchForm
+
 
 class SearchAndMenuCreateView(QuickSearchFormMixin, MenuMixin, CreateView):
     pass
@@ -20,9 +20,7 @@ class SearchAndMenuTemplateView(QuickSearchFormMixin, MenuMixin, TemplateView):
     pass
 
 class SitemapView(SearchAndMenuTemplateView):
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        quicksearchform = QuickSearchForm()
-        context.update({"quicksearchform": quicksearchform, "menu_left_sm": menu_items})
+        context.update({"menu_left_sm": menu_items})
         return context
