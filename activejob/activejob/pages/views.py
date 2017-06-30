d17c459c6f783ea75bc2a6462ef6f21f1efd8011
Arne <arne@yoshi.dynu.com>
2017-06-30 13:45:12 +0200

    working on menu for 'pages' page

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -5,14 +5,19 @@ from core.utils import build_main_menu
 
 class PageView(SearchAndMenuDetailView):
 
+    template_name = "web/pages/page.html"
+    model = Page
+
     def dispatch(self, request, *args, **kwargs):
         self.active_nodes = {
-            "top": "Page.menu_top_entry",
-            "left": "Page.menu_left_entry",
-            "sub": "Page.menu_left_sub_entry",
+            "top": Page.menu_top_entry,
+            "left": Page.menu_left_entry,
+            "sub": Page.menu_left_sub_entry,
         }
 
         return super().dispatch(request, *args, **kwargs)
 
-    template_name = "web/pages/page.html"
-    model = Page
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"top": Page.menu_top_entry, "left": Page.menu_left_entry, "sub": Page.menu_left_sub_entry})
+        return context
