8e35f1f7d21ac7d1c7d86d0843f99eaf2aeb16f8
Arne <arne@yoshi.dynu.com>
2017-07-01 01:38:38 +0200

    unelegant .. aber ein ganz anders problem... unsere menu-listen wachsen ins unermessliche... ein paar mal Stellenmarkt AÜ anklicken und mal die URL anschauen... :/

--- activejob/activejob/core/utils.py
+++ activejob/activejob/core/utils.py
@@ -12,6 +12,15 @@ def build_main_menu(active_nodes=None):
         if "sublist" in item:
             for subitem in item["sublist"]:
                 subitem["active"] = subitem["name"] == active_nodes.get("sub")
+                #need to do this here. if this happens in sitemenu.menu_items
+                #it tries to resolve reverse_lazy() to early and rises error
+                if subitem["name"] == "Stellenmarkt AÜ":
+                    subitem["url"] += "?aü"
+                if subitem["name"] == "Stellenmarkt PV":
+                    subitem["url"] += "?pv"
+                if subitem["name"] == "Stellenmarkt AV":
+                    subitem["url"] += "?av"
+
 
     return {
         "menu_top": menu_top,
