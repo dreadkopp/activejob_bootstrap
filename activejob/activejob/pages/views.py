fc9e3862a9d42b26fd318280f2ccf947476a3cb7
Arne <arne@yoshi.dynu.com>
2017-06-30 14:32:15 +0200

    This seems to work

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -10,18 +10,11 @@ class PageView(SearchAndMenuDetailView):
     model = Page
 
     def dispatch(self, request, *args, **kwargs):
-        p = get_object_or_404(Page, slug=kwargs["slug"])
+        p = get_object_or_404(Page, slug=kwargs['slug'])
         self.active_nodes = {
             "top": p.menu_top_entry,
             "left": p.menu_left_entry,
             "sub": p.menu_left_sub_entry,
         }
-
+        
         return super().dispatch(request, *args, **kwargs)
-
-#Debug
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        p = get_object_or_404(Page, slug=kwargs["slug"])
-        context.update({"top": p.menu_top_entry, "left": p.menu_left_entry, "sub": p.menu_left_sub_entry})
-        return context
