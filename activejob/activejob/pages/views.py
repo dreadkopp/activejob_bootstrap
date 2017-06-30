0d875cfc23155897cc7104d9f3a34428e5acfe1e
Arne <arne@yoshi.dynu.com>
2017-06-30 14:23:26 +0200

    This gives me a 'Key Error' for 'slug' :/

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -1,6 +1,7 @@
 from core.views import SearchAndMenuDetailView
 from .models import Page
 from core.utils import build_main_menu
+from django.shortcuts import get_object_or_404
 
 
 class PageView(SearchAndMenuDetailView):
@@ -9,15 +10,18 @@ class PageView(SearchAndMenuDetailView):
     model = Page
 
     def dispatch(self, request, *args, **kwargs):
+        p = get_object_or_404(Page, slug=kwargs["slug"])
         self.active_nodes = {
-            "top": Page.menu_top_entry,
-            "left": Page.menu_left_entry,
-            "sub": Page.menu_left_sub_entry,
+            "top": p.menu_top_entry,
+            "left": p.menu_left_entry,
+            "sub": p.menu_left_sub_entry,
         }
 
         return super().dispatch(request, *args, **kwargs)
 
+#Debug
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({"top": Page.menu_top_entry, "left": Page.menu_left_entry, "sub": Page.menu_left_sub_entry})
+        p = get_object_or_404(Page, slug=kwargs["slug"])
+        context.update({"top": p.menu_top_entry, "left": p.menu_left_entry, "sub": p.menu_left_sub_entry})
         return context
