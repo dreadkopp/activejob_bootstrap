ecbe8aa7952d99ef30345d3c867df309d1e677f6
Arne <arne@yoshi.dynu.com>
2017-06-30 18:20:16 +0200

    bigger textField for Page model + allow empty string for banner_slogan

--- activejob/activejob/pages/models.py
+++ activejob/activejob/pages/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=64)
     subtitle = models.CharField(max_length=64)
-    text = models.TextField(max_length=2000)
+    text = models.TextField(max_length=4000)
     banner_color = models.CharField(max_length=6)
-    banner_slogan = models.CharField(max_length=128)
+    banner_slogan = models.CharField(max_length=128,blank=True)
     menu_top_entry = models.CharField(max_length=64)
     menu_left_entry = models.CharField(max_length=64)
     menu_left_sub_entry = models.CharField(max_length=64)
