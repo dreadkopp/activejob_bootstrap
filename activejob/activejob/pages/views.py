d45ec538f3a9f10f2db24d5412dea84d33e3c117
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-30 18:00:23 +0200

    just stuff the stuff between some other stuff

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -1,7 +1,6 @@
 from core.views import SearchAndMenuDetailView
 from .models import Page
 from core.utils import build_main_menu
-from django.shortcuts import get_object_or_404
 
 
 class PageView(SearchAndMenuDetailView):
@@ -9,12 +8,15 @@ class PageView(SearchAndMenuDetailView):
     template_name = "web/pages/page.html"
     model = Page
 
-    def dispatch(self, request, *args, **kwargs):
-        p = get_object_or_404(Page, slug=kwargs['slug'])
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        p = self.object
         self.active_nodes = {
             "top": p.menu_top_entry,
             "left": p.menu_left_entry,
             "sub": p.menu_left_sub_entry,
         }
 
-        return super().dispatch(request, *args, **kwargs)
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
