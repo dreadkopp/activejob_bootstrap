a56dbec728d8d126e7b69c7355b5cb6c24fd889b
Daniel Musketa <daniel.musketa@activ-job.com>
2017-06-30 18:16:54 +0200

    always!

--- activejob/activejob/pages/views.py
+++ activejob/activejob/pages/views.py
@@ -8,14 +8,11 @@ class PageView(SearchAndMenuDetailView):
     template_name = "web/pages/page.html"
     model = Page
 
-    def get_object(self):
-        the_object = super().get_object()
-
-        p = the_object
+    def get_context_data(self, **kwargs):
+        p = self.object
         self.active_nodes = {
             "top": p.menu_top_entry,
             "left": p.menu_left_entry,
             "sub": p.menu_left_sub_entry,
         }
-
-        return the_object
+        return super().get_context_data(**kwargs)
