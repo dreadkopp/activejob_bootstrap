88178b72b4bfbaae824897d457ca4480aa1569a9
Daniel Musketa <daniel.musketa@activ-job.com>
2017-07-04 07:35:36 +0200

    make JobAdmin more usable

--- activejob/activejob/jobs/admin.py
+++ activejob/activejob/jobs/admin.py
@@ -12,11 +12,18 @@ from .models import (
 )
 
 
+class JobAdmin(admin.ModelAdmin):
+    list_display = ["pk", "title", "location", "contact_profile", "changed_at"]
+    ordering = ["pk"]
+    list_filter = ["is_intern"]
+    search_fields = ["title", "location"]
+
+
 admin.site.register(Category)
 admin.site.register(Company)
 admin.site.register(Contact)
 admin.site.register(ContactProfile)
 admin.site.register(Department)
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
 admin.site.register(Location)
 admin.site.register(State)
