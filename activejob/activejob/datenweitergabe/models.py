0d8026e0ba1f380c6ca6f04d98772197755875f7
Daniel Musketa <daniel.musketa@activ-job.com>
2017-07-05 00:18:32 +0200

    created model and migration for datenweitergabe

--- activejob/activejob/datenweitergabe/models.py
+++ activejob/activejob/datenweitergabe/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from jobs.models import Company
+
+
+class PendingConfirmation(models.Model):
+    slug = models.SlugField(primary_key=True, max_length=64)
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+    )
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.slug
+
+
+class Confirmation(models.Model):
+    pending_confirmation = models.ForeignKey(
+        PendingConfirmation,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    logdata = models.TextField()
+
+    def __str__(self):
+        return self.pending_confirmation.slug
