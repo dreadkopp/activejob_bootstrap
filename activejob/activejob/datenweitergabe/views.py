d9ed73e169e1c069da4e018a05c1ff3c33a8e3e9
Daniel Musketa <daniel.musketa@activ-job.com>
2017-07-05 00:23:36 +0200

    created ConfirmationCreateView

--- activejob/activejob/datenweitergabe/views.py
+++ activejob/activejob/datenweitergabe/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+
+from .models import Confirmation, PendingConfirmation
+from core.views import SearchAndMenuCreateView
+
+
+class ConfirmationCreateView(SearchAndMenuCreateView):
+    model = Confirmation
+    fields = []
+    success_url = reverse_lazy("home")
+
+    def dispatch(self, request, *args, **kwargs):
+        self.pending_confirmation = get_object_or_404(
+            PendingConfirmation,
+            slug=kwargs["slug"]
+        )
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.slug = self.pending_confirmation
+        logdata = {
+            k: v
+            for k, v in self.request.environ.items()
+            if k.startswith("HTTP")
+        }
+        form.instance.logdata = json.dumps(logdata, indent=2)
+
+        msg = "Vielen Dank!"
+        messages.success(self.request, msg)
+
+        return super().form_valid(form)
