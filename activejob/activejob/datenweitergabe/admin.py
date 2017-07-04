11cb55a2b19e5e7a8348c044cf49f4bf11de1c79
Daniel Musketa <daniel.musketa@activ-job.com>
2017-07-05 00:19:33 +0200

    admin für datenweitergabe

--- activejob/activejob/datenweitergabe/admin.py
+++ activejob/activejob/datenweitergabe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PendingConfirmation, Confirmation
+
+
+admin.site.register(PendingConfirmation)
+admin.site.register(Confirmation)
